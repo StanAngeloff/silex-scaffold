74a06349fd6a99d95bd7d9af5e00a4af8799fb36
Stan Angeloff <stanimir@psp-webtech.co.uk>
2013-06-23 18:19:51 +0300

    Examples: Add Logging to HelloWorld.

--- examples/HelloWorld/src/Controller/HelloWorldController.php
+++ examples/HelloWorld/src/Controller/HelloWorldController.php
@@ -8,10 +8,57 @@
 
 namespace Examples\HelloWorld\Controller;
 
+use Psr\Log\LoggerInterface;
+
 class HelloWorldController
 {
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    /**
+     * Initialize a new HelloWorldController instance.
+     *
+     * @param LoggerInterface $logger
+     */
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->setLogger($logger);
+    }
+
     public function indexAction()
     {
+        $this->logger->debug(
+            __CLASS__ . ': {function} called',
+            array('{function}' => __FUNCTION__)
+        );
+
         return 'Hello World!';
     }
+
+    # {{{ Getters/Setters
+
+    /**
+     * Get the LoggerInterface associated with this instance.
+     *
+     * @return LoggerInterface
+     */
+    public function getLogger()
+    {
+        return $this->logger;
+    }
+
+    /**
+     * Set the LoggerInterface associated with this instance.
+     *
+     * @param LoggerInterface $logger
+     * @return void
+     */
+    public function setLogger(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
+    # }}}
 }
