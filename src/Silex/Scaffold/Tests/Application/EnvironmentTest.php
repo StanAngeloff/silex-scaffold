a2b48a5eb6fa4cd6bea2079888e3bc36ea9a486c
Stan Angeloff <stanimir@psp-webtech.co.uk>
2013-06-23 12:35:49 +0300

    Tests: Move Environment tests to a new file, #nfc.

---
+++ src/Silex/Scaffold/Tests/Application/EnvironmentTest.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * (c) PSP UK Group Ltd. <hello@psp-group.co.uk>
+ *
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace Silex\Scaffold\Tests\Application;
+
+use Silex\Scaffold\Tests\AbstractTestCase;
+
+use Silex\Scaffold\Application\Environment;
+
+final class EnvironmentTest extends AbstractTestCase
+{
+    public function testEnv()
+    {
+        putenv('SCAFFOLD_APP_ENV=');
+        unset ($_SERVER['SCAFFOLD_APP_ENV']);
+        unset ($_SERVER['REDIRECT_SCAFFOLD_APP_ENV']);
+        $_SERVER['HTTP_HOST'] = 'scaffold.app';
+
+        $app = $this->createApplication();
+        $this->assertEquals(
+            Environment::ENV_PRODUCTION,
+            $app['env'],
+            'expect the default environment to be production'
+        );
+
+        putenv('SCAFFOLD_APP_ENV=' . ($env = Environment::ENV_TESTING));
+        $this->assertEquals(
+            $env,
+            $app['env'],
+            'expect the environment to be loaded from $_ENV'
+        );
+        putenv('SCAFFOLD_APP_ENV=');
+
+        $_SERVER['SCAFFOLD_APP_ENV'] = $env;
+        $this->assertEquals(
+            $env,
+            $app['env'],
+            'expect the environment to be loaded from $_SERVER'
+        );
+        unset ($_SERVER['SCAFFOLD_APP_ENV']);
+
+        $_SERVER['REDIRECT_SCAFFOLD_APP_ENV'] = $env;
+        $this->assertEquals(
+            $env,
+            $app['env'],
+            'expect the environment to be loaded from $_SERVER when mod_rewrite is activated'
+        );
+        unset ($_SERVER['REDIRECT_SCAFFOLD_APP_ENV']);
+
+        $_SERVER['HTTP_HOST'] = 'hello.xip.io';
+        $this->assertEquals(
+            Environment::ENV_DEVELOPMENT,
+            $app['env'],
+            'expect the fallback environment on a xip.io domain to be development'
+        );
+
+        $app = $this->createApplication(null, array(array(), ($env = Environment::ENV_TESTING)));
+        $this->assertEquals(
+            $env,
+            $app['env'],
+            'expect the environment to be set from the __constructor'
+        );
+    }
+}
--- src/Silex/Scaffold/Tests/ApplicationTest.php
+++ src/Silex/Scaffold/Tests/ApplicationTest.php
@@ -9,7 +9,6 @@
 namespace Silex\Scaffold\Tests;
 
 use Silex\Scaffold\Application;
-use Silex\Scaffold\Application\Environment;
 
 final class ApplicationTest extends AbstractTestCase
 {
@@ -24,58 +23,5 @@ final class ApplicationTest extends AbstractTestCase
 
         $app->setName($appName = 'my_app');
         $this->assertEquals($appName, $app->getName(), 'expect the app name to be changed');
-    }
-
-    public function testEnv()
-    {
-        putenv('SCAFFOLD_APP_ENV=');
-        unset ($_SERVER['SCAFFOLD_APP_ENV']);
-        unset ($_SERVER['REDIRECT_SCAFFOLD_APP_ENV']);
-        $_SERVER['HTTP_HOST'] = 'scaffold.app';
-
-        $app = $this->createApplication();
-        $this->assertEquals(
-            Environment::ENV_PRODUCTION,
-            $app['env'],
-            'expect the default environment to be production'
-        );
-
-        putenv('SCAFFOLD_APP_ENV=' . ($env = Environment::ENV_TESTING));
-        $this->assertEquals(
-            $env,
-            $app['env'],
-            'expect the environment to be loaded from $_ENV'
-        );
-        putenv('SCAFFOLD_APP_ENV=');
-
-        $_SERVER['SCAFFOLD_APP_ENV'] = $env;
-        $this->assertEquals(
-            $env,
-            $app['env'],
-            'expect the environment to be loaded from $_SERVER'
-        );
-        unset ($_SERVER['SCAFFOLD_APP_ENV']);
-
-        $_SERVER['REDIRECT_SCAFFOLD_APP_ENV'] = $env;
-        $this->assertEquals(
-            $env,
-            $app['env'],
-            'expect the environment to be loaded from $_SERVER when mod_rewrite is activated'
-        );
-        unset ($_SERVER['REDIRECT_SCAFFOLD_APP_ENV']);
-
-        $_SERVER['HTTP_HOST'] = 'hello.xip.io';
-        $this->assertEquals(
-            Environment::ENV_DEVELOPMENT,
-            $app['env'],
-            'expect the fallback environment on a xip.io domain to be development'
-        );
-
-        $app = $this->createApplication(array(), ($env = Environment::ENV_TESTING));
-        $this->assertEquals(
-            $env,
-            $app['env'],
-            'expect the environment to be set from the __constructor'
-        );
     }
 }
